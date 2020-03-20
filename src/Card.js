a240adceee9ca937b05d462271d0d0f5d9349701
John Doe <andres.dana.maria@gmail.com>
2020-03-20 16:55:27 +0100

    Adds project 01

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Card.css';
+
+const Card = () => {
+    return (
+        <div class='product'>
+            <img alt='foto' src=''></img>
+            <div>
+                <h2>Title</h2>
+                <p>Description</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div class='fixheader'>
+            <img alt='logo' src=''></img>
+            <button>Home</button>
+            <button>About</button>
+            <button>Contact</button>
+        </div>
+    );
+}
+
+export default Header;
